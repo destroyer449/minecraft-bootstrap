e1fc5a388957487b77f1d54f9a1b8b5730e2efd4
Dominic Adamson <adamson.dom@gmail.com>
2021-02-21 16:09:07 -0700

    fixed install

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 stow .
 cd ..
+echo "installing at:" && pwd
 
 [ ! -f paperclip.jar ] && curl -o paperclip.jar "https://papermc.io/api/v1/paper/1.16.5/latest/download"
 echo "eula=true" > eula.txt
 [ ! -d plugins ] && mkdir plugins
 cd plugins
-[ ! -f LuckPerms.jar ] && curl -o LuckPerms.jar "https://ci.lucko.me/job/LuckPerms/1281/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.2.115.jar" \
-    && mkdir LuckPerms
+[ ! -f LuckPerms.jar ] && curl -o LuckPerms.jar "https://ci.lucko.me/job/LuckPerms/1281/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.2.115.jar"
 [ ! -f Vault.jar ] && wget "https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar"
 [ ! "$(echo EssentialsX*)" != 'EssentialsX*' ] && git clone https://github.com/EssentialsX/essentials.git \
     && cd essentials && ./gradlew build \

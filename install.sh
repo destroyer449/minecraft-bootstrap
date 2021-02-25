a2075cece9f0879503cad6d19267f6dce630bf41
Dominic Adamson <adamson.dom@gmail.com>
2021-02-24 18:28:44 -0700

    additional verbosity in install

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 set -euo pipefail
 stow .
-cd ..
-echo "installing at:" && pwd
+installdir=$PWD/..
+cd $installdir
+echo "installing at: $installdir"
+plugdir=$installdir/plugins
 
 [ ! -f paperclip.jar ] && curl -o paperclip.jar "https://papermc.io/api/v1/paper/1.16.5/latest/download"
 echo "eula=true" > eula.txt
-[ ! -d plugins ] && mkdir plugins
-cd plugins
-[ ! -f LuckPerms.jar ] && curl -o LuckPerms.jar "https://ci.lucko.me/job/LuckPerms/1281/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.2.115.jar"
-[ ! -f Vault.jar ] && wget "https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar"
+[ ! -d $plugdir ] && mkdir $plugdir
+cd $plugdir && pwd
+[ ! -f $plugdir/LuckPerms.jar ] && curl -o $plugdir/LuckPerms.jar "https://ci.lucko.me/job/LuckPerms/1281/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.2.115.jar"
+[ ! -f $plugdir/Vault.jar ] && wget -O $plugdir/Vault.jar "https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar"
 [ ! "$(echo EssentialsX*)" != 'EssentialsX*' ] && git clone https://github.com/EssentialsX/essentials.git \
-    && cd essentials && ./gradlew build \
-    && mv jars/* .. && cd .. && rm -rf essentials && rm EssentialsXXMP* EssentialsXGeoIP*
+    && cd $plugdir/essentials && ./gradlew build \
+    && mv jars/* .. && cd $plugdir && rm -rf essentials && rm EssentialsXXMP* EssentialsXGeoIP*

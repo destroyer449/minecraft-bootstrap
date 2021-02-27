935ca41f5d0bc95689c14f0daaa0f5c8aa96e499
Dominic Adamson <adamson.dom@gmail.com>
2021-02-27 12:58:34 -0700

    Fixes to install script

--- install.sh
+++ install.sh
@@ -6,12 +6,37 @@ cd $installdir
 echo "installing at: $installdir"
 plugdir=$installdir/plugins
 
+# Install newest verison of paper
 [ ! -f paperclip.jar ] && curl -o paperclip.jar "https://papermc.io/api/v1/paper/1.16.5/latest/download"
+# 
+# Allow paper to run by agreeing to EULA
 echo "eula=true" > eula.txt
+
+# Installing plugins
 [ ! -d $plugdir ] && mkdir $plugdir
-cd $plugdir && pwd
-[ ! -f $plugdir/LuckPerms.jar ] && curl -o $plugdir/LuckPerms.jar "https://ci.lucko.me/job/LuckPerms/1281/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.2.115.jar"
-[ ! -f $plugdir/Vault.jar ] && wget -O $plugdir/Vault.jar "https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar"
+cd $plugdir
+
+# Plugins to install from SpiGet
+# Each line specifies the plugin to download, in the format:
+# NAME RESOURCE_NUMBER
+# Resource numbers can be found from searching spiget, or in the spigot url for the plugin
+# TODO Handling files in forms other than *.jar
+# TODO Support BukGet
+PKGS=(    
+    "Plugman 88135"
+    "LuckPerms 28140"
+    "Vault 34315"
+    "Dynmap 274"
+    "Dynmap-structures 39534"
+    "LiveAtlas 86939"
+
+)
+
+# TODO Optional build steps
+for PKG in "${PKGS[@]}"; do
+    [ ! -f $plugdir/"${PKG%% *}".jar ] && wget -O "${PKG%% *}".jar http://api.spiget.org/v2/resources/"${PKG##* }"/download
+done
+
 [ ! "$(echo EssentialsX*)" != 'EssentialsX*' ] && git clone https://github.com/EssentialsX/essentials.git \
     && cd $plugdir/essentials && ./gradlew build \
     && mv jars/* .. && cd $plugdir && rm -rf essentials && rm EssentialsXXMP* EssentialsXGeoIP*
